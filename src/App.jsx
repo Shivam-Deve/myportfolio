9fcf6b1cfc932986d7d2fa73af0862a9f16bec1d
Shivam <smshivammishra1998@gmail.com>
2022-12-26 16:31:09 +0530

    app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from './components/contact/Contact';
 import Intro from './components/into/Intro';
 import Navbar from './components/navbar/Navbar';
 import ProductList from './components/productList/ProductList';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
